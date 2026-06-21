f2fc55656269fe8ff7ab46e91c473731ebb7008f
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-06-21 09:58:42 +0300

    feat: product and category model + migrate

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,2 +1,33 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
+
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length=250)
+
+
+
+    def __str__(self):
+        return self.cat_name
+
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)]
+    )
+    description = models.CharField(max_length=500)
+    category = models.ManyToManyField(Category)
+    image = models.CharField(max_length=500, null=True, blank=True)
+    rating = models.DecimalField(
+       max_digits=2,
+       decimal_places=1,
+       validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+
+    def __str__(self):
+        return self.title 
