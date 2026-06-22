9a42da495928b17931aaac211c0d953aa700b7d7
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-06-22 09:42:36 +0300

    feat: add Category Serializer

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -3,8 +3,13 @@ from .models import Product, Category
 
 
 class ProductSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Product
         fields = '__all__'
         extra_kwargs = {'category': {'read_only': True}}
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
