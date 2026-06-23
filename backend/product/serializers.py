d337a5ca063e51e262884d8f4bd9f7bd3c1b394c
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-06-23 13:08:36 +0300

    feat: serializers Tag serializer

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
-from .models import Product, Category
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
-        extra_kwargs = {'category': {'read_only': True}}
+from .models import Product, Category, Tag
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True, read_only=True, source='category')
+    all_tags = TagSerializer(many=True, read_only=True, source='tags')
+    
+    class Meta:
+        model = Product
+        exclude=('category','tags',)
+      
+
