725e659c0f9d1c6eb2a37904fbddaa83724b31d5
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-06-26 16:30:11 +0300

    feat: wrap routes with CtxProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,21 @@ import ProductsPage from "./pages/ProductsPage";
 import Cart from "./pages/Cart";
 import EmptyCartPage from "./pages/EmptyCartPage";
 import NotFound from "./pages/NotFound";
+import CategoriesPage from "./pages/CategoriesPage";
+import Header from "./components/UI/Header";
+import { ProductsContextProvider } from "./context/ProductsContext";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<ProductsPage />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/empty-cart" element={<EmptyCartPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <ProductsContextProvider>
+      <Routes>
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/empty-cart" element={<EmptyCartPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </ProductsContextProvider>
   );
 };
 
